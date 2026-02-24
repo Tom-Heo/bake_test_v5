2e05021bae9bb2cf78dbd94732220bcc01307e45
tomheo <tom@generativelab.co.kr>
2026-02-24 17:17:25 +0900

    6.6.4

--- core/heo.py
+++ core/heo.py
@@ -303,10 +303,10 @@ class Heo:
 
             charbonnier = torch.sqrt(diff**2 + self.epsilon_char**2)
 
-            sharp_loss = (torch.log(1 + 20.0 * charbonnier / self.epsilon)) / 20.0
+            sharp_loss = (torch.log(1 + 500.0 * charbonnier / self.epsilon)) / 500.0
 
-            l2_loss = (20.0 * diff) ** 2 / 20.0
+            l2_loss = (500.0 * diff) ** 2 / 500.0
 
-            loss = (torch.where(abs_diff <= 0.05, sharp_loss, l2_loss)) / 20.0
+            loss = (torch.where(abs_diff <= 0.002, sharp_loss, l2_loss)) / 500.0
 
             return loss.mean()
